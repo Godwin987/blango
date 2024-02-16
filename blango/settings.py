e1430044682ecb74b766689d1c2dcdad1b65261e
Bassey Godwin <basseygodwin629@gmail.com>
2024-02-16 12:36:59 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -137,6 +137,14 @@ class Dev(Configuration):
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+    # Password Hashers
+    PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
     # Logging Configuration
     LOGGING = {
     "version": 1,
