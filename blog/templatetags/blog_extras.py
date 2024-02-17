639c7cc5cadaec2f6e78ef5ba2e34450fea7fc8e
Bassey Godwin <basseygodwin629@gmail.com>
2024-02-17 00:00:36 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -5,10 +5,12 @@ from django.utils.safestring import mark_safe
 from django import template
 from django.http import HttpRequest
 from blog.models import Post
+import logging
 
 request = HttpRequest()
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 user_model = get_user_model()
 
 # @register.filter()
@@ -77,4 +79,5 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
   posts = Post.objects.exclude(id=post.id).order_by('-published_at')[:5]
+  logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
   return {'title': 'Recent Posts', 'posts':posts}
